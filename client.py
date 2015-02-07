ba9988eabdab729dc88128f9fffad947bf931263
Jonathan Eustace <jeustace@tangledpixel.ca>
2015-02-07 09:37:54 -0800

    test

--- client.py
+++ client.py
@@ -3,17 +3,18 @@ from socket import *
 if __name__ == '__main__':
  
     host = 'localhost'
-    port = 55567
+    port = 55568
     buf = 1024
  
     addr = (host, port)
  
     clientsocket = socket(AF_INET, SOCK_STREAM)
- 
+  
+   //test
     clientsocket.connect(addr)
- 
+    data = raw_input(">> ")
     while 1:
-        data = raw_input(">> ")
+      
         if not data:
             break
         else:
