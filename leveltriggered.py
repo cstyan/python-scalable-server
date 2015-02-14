edfd1d04d27582b2707a03ed5be1cd8ff765a072
Jonathan Eustace <jeustace@tangledpixel.ca>
2015-02-13 19:48:21 -0800

    Mac is now garbage in my books.

--- leveltriggered.py
+++ leveltriggered.py
@@ -1,57 +1,56 @@
-import socket, select
+from socket import *
+import select
+import threading
 
 connectionCount = {}
 
 def setup(host, port, buffer, threads):
 	epollCollection = []
-	
-
 	addr = (host, port)
- 
-    serversocket = socket(AF_INET, SOCK_STREAM)
+ 	serversocket = socket(AF_INET, SOCK_STREAM)
 
 
-     for x in range(threads):
-     	#create an epoll object for each thread
-     	epoll = select.epoll
+	for x in range(threads):
+		#create an epoll object for each thread
+		epoll = select.epoll()
      	epollCollection[x] = epoll
      	connectionCount.update({x:0})
      	thread(threadNum, epoll)
- 
-    serversocket.bind(addr)
- 
-    serversocket.listen(10000)
 
-    try:
-    	while 1:
-    		clientsocket, clientaddr = serversocket.accept()
+	serversocket.bind(addr)
+	serversocket.listen(10000)
 
-    		threadWithLowestNumberOfConnections = min(connectionCount, key=connectionCount.get)
+	try:
+		while 1:
+			clientsocket, clientaddr = serversocket.accept()
+			clientsocket.setblocking(0)
+			threadWithLowestNumberOfConnections = min(connectionCount, key=connectionCount.get)
 
-    		epollCollection[threadWithLowestNumberOfConnections].register(clientsocket.fileno(), select.EPOLLIN)
-    	
+			epollCollection[threadWithLowestNumberOfConnections].register(clientsocket.fileno(), select.EPOLLIN)
 
-    finally:
-   
-    	serversocket.close()	
+
+	finally:
+		serversocket.close()	
 
 
 def thread(threadNum, epollObj):
-	epoll = select.epoll()
-	epoll.register()
+	buf = 1024
 	while 1:
-		events = epoll.poll(5)
+		events = epollObj.poll(-1)
 		# epoll level triggered on threads collection
-
+		for fileno, event in events:
+			if event & select.EPOLLIN:
+				data = fileno.recv(buf)
+				print buff
+				fileno.send(data)
 		# on unblock call messaging (read from socket, echo back)
 
-def accept:
+#def accept:
 
-def messaging:
+#def messaging:
 
 
 
 
 if __name__ == '__main__':
-
-
+	setup('localhost', 7000, 1024, 8)
