bd1478cfe0247c83bdf99add92ead5774f0e99a8
Callum Styan <callumstyan@gmail.com>
2015-02-21 19:02:39 -0800

    testing client

--- client.py
+++ client.py
@@ -1,42 +1,116 @@
 from socket import *
+import datetime
 import threading
 import time
 import datetime
 import random
+import sys
+import getopt
 
+global serverIP
+global port
+global buf #buffer size
+global msg #num of msgs to send
+global clients
+global msgStr
+global times
+global fileName
+msgStr = ""
+times = {}
+
+def genMsg():
+    global msgStr
+    i = 0
+    while i < buf:
+        msgStr += 'a'
+        i += 1
  
 def handleTheSocket(clientNumber):
-    host = 'localhost'
-    port = 8005
-    buf = 2048
-    #clientNumber = 5
-    addr = (host, port)
- 
+    global times
+    global serverIP
+    addr = (serverIP, port) 
     clientsocket = socket(AF_INET, SOCK_STREAM)
     clientsocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     clientsocket.connect(addr)
-  
-    while 1:
-      
-      data =  "hi I'm " + str(clientsocket.getsockname()) +"373248644238091033361171340508505162629570915145962037766433373137770359367676167852752348371395682890859173122892676240122689740006895490189156549494689538759749967414623425907742325632425691232808842595508894358621128519780747236911707915627432006418367432030047000243176184421517923026449506670356"
-      #raw_input("Send the server a message! >>>")
-      clientsocket.send(data)
-      print "sent data"
-      data = clientsocket.recv(buf)
-      print data + '\n'
-      t = random.randint(0, 0)
-      time.sleep(t)
+
+    i = 0
+    start = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S%f')
+    print "starting to send messages"
+    print len(msgStr)
+    while i < msg:
+        clientsocket.send(msgStr)
+        print "sent"
+        data = clientsocket.recv(buf)
+        print "recvd"
+        i += 1
+        print str(i)
+
+    end = datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S%f')
+    duration = int(end) - int(start)
+    times.update({threading.current_thread():duration})
+
+def main(argv):
+    global serverIP
+    global port
+    global buf
+    global msg
+    global clients
+    global fileName
+
+    print " getting arguments"
+
+    try:
+        opts, args = getopt.getopt(argv, "s:p:b:n:c:o:h",["serverIP=","port=","bufferSize=", "numMsg=" "numClients=", "output=", "help"])
+    except getopt.GetoptError:
+        print 'client.py -s <serverIP> -p <port> -b <bufferSize> -n <numMsg> -c <numClients> -o <output>'
+        sys.exit(2)
+    
+    if len(sys.argv) < 6:
+        print 'client.py -s <serverIP> -p <port> -b <bufferSize> -n <numMsg> -c <numClients> -o <output>'
+        sys.exit()
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print 'client.py -s <serverIP> -p <port> -b <bufferSize> -n <numMsg> -c <numClients> -o <output>'
+            sys.exit()
+            port = int(arg)
+        elif opt in ("-s", "--serverIP"):
+            serverIP = arg
+        elif opt in ("-p", "--port"):
+            port = int(arg)
+        elif opt in ("-b", "--bufferSize"):
+            buf = int(arg)
+        elif opt in ("-n", "--numMsg"):
+            msg = int(arg)
+        elif opt in ("-c", "--numClients"):
+            clients = int(arg)
+        elif opt in("-o", "--output"):
+            fileName = arg
 
 if __name__ == '__main__':
- 
-        threads = []
+    main(sys.argv[1:])
+    threads = []
 
-        for x in range(10):
-            thread = threading.Thread(target = handleTheSocket, args = [x])
-            thread.start()
-            threads.append(thread)
-           
+    print "serverIP: %s" % serverIP
+    print "port: %d" % port
+    print "buffer: %d" % buf
+    print "numMsgs: %d" % msg
+    print "numClients: %d" % clients
+    #print "buffer: %d" % buf
+    genMsg()
 
-        for thread in threads:
-            thread.join()
-      
+    for x in range(clients):
+        thread = threading.Thread(target = handleTheSocket, args = [x])
+        thread.start()
+        threads.append(thread)
+       
+    for thread in threads:
+        thread.join()
+    
+    print "threads are finished"
+    with open(fileName, 'w') as f:  
+        for key in times:
+            f.write(str(times[key]) + "\n")
+            print times[key]
+
+    print "done"
